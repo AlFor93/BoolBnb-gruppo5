a20c9d8aad92fe1a6aface541b535bc2299e82c8
giambal <gimmy_1996@hotmail.it>
2019-06-11 17:02:23 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,19 @@
                     You are logged in!
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header">fatta da giammarco</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
         </div>
     </div>
 </div>
