fb7a61dd6d240ec5fbc9f8d97ece89ff56a5d5a4
stg25 <stefanoghizzi@yahoo.com>
2019-06-17 16:12:43 +0200

    updated flats controller

---
+++ resources/views/page/flats.blade.php
@@ -0,0 +1,9 @@
+@extends('index.flats')
+
+@section('content')
+
+  <div class="main-flats">
+    <h1>questo è il corpo FLATS</h1>
+  </div>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/BoolBnB' , 'BoolHomeController@index')->name('BoolHome');
+
+Route::get('/BoolBnB/Flats' , 'BoolFlatsController@index')->name('BoolFlats');
