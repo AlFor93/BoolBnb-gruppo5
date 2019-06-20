9f2fc81084533fd3bfcf69613789c13954ea45d4
stg25 <stefanoghizzi@yahoo.com>
2019-06-21 00:28:08 +0200

    service add

--- resources/views/page/flats.blade.php
+++ resources/views/page/flats.blade.php
@@ -24,6 +24,7 @@
     </div>
     <div class="second-container">
       <div class="left">
+        {{-- Top left second container --}}
         <div class="top">
           <div class="info">
             <h1>Flat/Apartment name, Location</h1>
@@ -36,17 +37,37 @@
             <span>username</span>
           </div>
         </div>
+        {{-- Center left second container --}}
         <div class="center">
           <div class="service">
-
+            <div class="left">
+              <i class="fas fa-home"></i>
+            </div>
+            <div class="right">
+              <h4>Intero appartamento</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+            </div>
           </div>
           <div class="service">
-
+            <div class="left">
+              <i class="fas fa-spray-can"></i>
+            </div>
+            <div class="right">
+              <h4>Pulizia perfetta</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+            </div>
           </div>
           <div class="service">
-
+            <div class="left">
+              <i class="fas fa-key"></i>
+            </div>
+            <div class="right">
+              <h4>Ottima esperienza di check-in</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt,consectetur adipisicing elit, sed do eiusmod tempor incididunt</p>
+            </div>
           </div>
         </div>
+        {{-- Bottom left second container --}}
         <div class="bottom">
 
         </div>
