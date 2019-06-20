e63ef8890f7262eab641893908535c854dbad06b
giambal <gimmy_1996@hotmail.it>
2019-06-20 16:56:51 +0200

    added return view flat_sponsor

--- app/Http/Controllers/BoolHomeController.php
+++ app/Http/Controllers/BoolHomeController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use App\Http\Controller\BoolHomeController;
+use Illuminate\Support\Facades\DB;
 use App\Flat;
+
 
 class BoolHomeController extends Controller
 {
 
   public function index()
   {
-      
-      return view('page.home');
+      // $spFlats=Flat::has('created_at')->take(6)->get();
+
+      $flats=DB::table('flats')
+                ->join('flat_sponsor','flats.id','=','flat_sponsor.flat_id')
+                ->join('images','flats.id','=','images.flat_id')
+                ->get();
+
+      return view('page.home',compact('flats'));
+      // dd($flats);
+
   }
 }
+
+//
+// SELECT * FROM flats
+// JOIN flat_sponsor ON flats.id=flat_sponsor.flat_id
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -8,4 +8,15 @@
     <h1>questo è il corpo</h1>
   </div>
 
+  @foreach($flats as $flat)
+
+      <div class="flats">
+        <h1>{{$flat->name}} {{$flat->address}}</h1><br>
+        <div class="">
+          <img src="{{$flat->img_file}}" alt="">
+        </div>
+      </div>
+
+
+  @endforeach
 @stop
