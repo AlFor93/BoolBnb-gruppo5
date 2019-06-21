b1047be0fe422ee0eb9aa5d9ff247ace124388db
giambal <gimmy_1996@hotmail.it>
2019-06-21 15:08:52 +0200

    conflict resolved

--- app/Http/Controllers/BoolHomeController.php
+++ app/Http/Controllers/BoolHomeController.php
@@ -15,14 +15,33 @@ class BoolHomeController extends Controller
   {
       // $spFlats=Flat::has('created_at')->take(6)->get();
 
-      $flats = DB::table('flats')
+      $flats=DB::table('flats')
                 ->join('flat_sponsor','flats.id','=','flat_sponsor.flat_id')
                 ->join('images','flats.id','=','images.flat_id')
                 ->get();
 
       return view('page.home',compact('flats'));
+      // function isPresent($elem,$flats)
+      // {
+      //   $finded=false;
+      //   for ($i=0; $i < count($flats) ; $i++) {
+      //
+      //     if ($elem==$flats[$i]) {
+      //
+      //       $finded=true;
+      //     }
+      //   }
+      //   return $finded;
+      // }
 
-      dd($flats);
+
+
+  }
+
+
+
+
+
 
 
 }
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -11,7 +11,6 @@
   @foreach($flats as $flat)
 
       <div class="flats">
-
         <h1>{{$flat->name}} {{$flat->address}}</h1><br>
         <div class="">
           <img src="{{$flat->img_file}}" alt="">
